534666c2dc2099f2906b4ef019f7533694aca3f4
maleacristina <maleacristinaecaterina@gmail.com>
2018-10-08 22:13:54 +0300

    Filtering data Cloud Firestore

--- EmployeesApp/filter.js
+++ EmployeesApp/filter.js
@@ -1,25 +1,72 @@
 $(document).ready(function() {
             $('#onlyMalesFilter').click(function(){
-                console.log('onlyMalesFilter Filter executed');
+                //console.log('onlyMalesFilter Filter executed');
+                employeesRef.where("gender", "==", "Male")
+                .onSnapshot(function(querySnapshot){
+                    LoadTableData(querySnapshot);
+                });
             });
 
             $('#fullTimeFilter').click(function(){
-                console.log('fullTimeFilter Filter executed');
+                //console.log('fullTimeFilter Filter executed');
+                employeesRef.where("isFullTime", "==", true)
+                .onSnapshot(function(querySnapshot) {
+                    LoadTableData(querySnapshot);
+                });
             });
 
             $('#olderThenFilter').click(function(){
-                console.log('olderThenFilter Filter executed');
+                //console.log('olderThenFilter Filter executed');
+                employeesRef.where("age", ">=", 30)
+                .onSnapshot(function(querySnapshot) {
+                    LoadTableData(querySnapshot);
+                });
             });
 
             $('#ageBetweenFilter').click(function(){
-                console.log('ageBetweenFilter Filter executed');
+                //console.log('ageBetweenFilter Filter executed');
+                employeesRef.where("age", ">=", 35).where("age", "<=", 50)
+                .onSnapshot(function(querySnapshot) {
+                    LoadTableData(querySnapshot);
+                });
             });
 
             $('#yearsOfExperienceFilter').click(function(){
-                console.log('yearsOfExperienceFilter Filter executed');
+                //console.log('yearsOfExperienceFilter Filter executed');
+                employeesRef.where("gender", "==", "Female")
+                employeesRef.where("yearsOfExperience", ">=", 5).where("yearsOfExperience", "<=", 10)
+                .onSnapshot(function(querySnapshot) {
+                    LoadTableData(querySnapshot);
+                });
             });
 
             $('#clearFilter').click(function(){
-                console.log('clearFilter Filter executed');
+                //console.log('clearFilter Filter executed');
+                employeesRef.get().then(function(querySnapshot) {
+                    LoadTableData(querySnapshot);
+                });
             });
 });
+
+db.collection("employees").onSnapshot(function(snapShot){
+    // console.log("something changed");
+    LoadTableData(snapShot);
+})
+function LoadTableData(querySnapshot){
+    var tableRow='';
+    querySnapshot.forEach(function(doc) {
+        var document = doc.data();
+        tableRow +='<tr>';
+        tableRow += '<td class="fname">' + document.fName + '</td>';
+        tableRow += '<td class="lname">' + document.lName + '</td>';
+        tableRow += '<td class="email">' + document.email + '</td>';
+        tableRow += '<td class="age">' + document.age + '</td>';
+        tableRow += '<td class="gender">' + document.gender + '</td>';
+        tableRow += '<td class="yearsofexperience">' + document.yearsOfExperience + '</td>';
+        tableRow += '<td class="isfulltime">' + document.isFullTime + '</td>';
+        tableRow += '<td class="editEmployee"><i class="fa fa-pencil" aria-hidden="true" style="color:green"></i></td>'
+        tableRow += '<td class="deleteEmployee"><i class="fa fa-trash" aria-hidden="true" style="color:red"></i></td>'
+        tableRow += '</tr>';
+    });
+    $('tbody.tbodyData').html(tableRow);
+  }
--- EmployeesApp/script.js
+++ EmployeesApp/script.js
@@ -1,30 +1,5 @@
 $(document ).ready(function() {
     //get all the data on app startup
-    LoadData();
-
-    function LoadData(){
-        employeesRef.get().then(function(querySnapshot) {
-            LoadTableData(querySnapshot)
-        });
-      }
-      function LoadTableData(querySnapshot){
-        var tableRow='';
-        querySnapshot.forEach(function(doc) {
-            var document = doc.data();
-            tableRow +='<tr>';
-            tableRow += '<td class="fname">' + document.fName + '</td>';
-            tableRow += '<td class="lname">' + document.lName + '</td>';
-            tableRow += '<td class="email">' + document.email + '</td>';
-            tableRow += '<td class="age">' + document.age + '</td>';
-            tableRow += '<td class="gender">' + document.gender + '</td>';
-            tableRow += '<td class="yearsofexperience">' + document.yearsOfExperience + '</td>';
-            tableRow += '<td class="isfulltime">' + document.isFullTime + '</td>';
-            tableRow += '<td class="editEmployee"><i class="fa fa-pencil" aria-hidden="true" style="color:green"></i></td>'
-            tableRow += '<td class="deleteEmployee"><i class="fa fa-trash" aria-hidden="true" style="color:red"></i></td>'
-            tableRow += '</tr>';
-        });
-        $('tbody.tbodyData').html(tableRow);
-      }
 
     $('#createEmployee').click(function(){
         $('.employeeForm').css("display", "block");
@@ -59,7 +34,6 @@ $(document ).ready(function() {
             .then(function(docRef) {
                 $('#operationStatus').html('<div class="alert alert-success"><strong>Success!</strong> Employee was created!</div>').delay(2500).fadeOut('slow');
                 $('.employeeForm').css("display", "none");
-                LoadData();
            })
            .catch(function(error) {
                $('#operationStatus').html('<div class="alert alert-danger"><strong>Error!</strong> Employee was not created!</div>').delay(2500).fadeOut('slow');
@@ -82,7 +56,6 @@ $(document ).ready(function() {
            .then(function() {
                 $('#operationStatus').html('<div class="alert alert-success"><strong>Success!</strong> Employee was updated.</div>').delay(2500).fadeOut('slow');
                 $('.employeeForm').css("display", "none");
-                LoadData();
             })
             .catch(function(error) {
                 $('#operationStatus').html('<div class="alert alert-danger"><strong>Failure!</strong> Employee could not be updated.</div>').delay(2500).fadeOut('slow');
@@ -120,7 +93,6 @@ $(document ).ready(function() {
         var docuName = fName.charAt(0)+"."+lName;
         db.collection("employees").doc(docuName).delete().then(function() {
             $('#operationStatus').html('<div class="alert alert-success"><strong>Success!</strong> Employee was deleted.</div>').delay(2500).fadeOut('slow');
-            LoadData();
         }).catch(function(error) {
             $('#operationStatus').html('<div class="alert alert-danger"><strong>Failure!</strong> Employee was not deleted.</div>').delay(2500).fadeOut('slow');
         });
